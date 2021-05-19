9aa7fc5ce8763f2d9600ce0c8fc7e5e5e0c4438b
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-19 17:35:22 +0900

    My debate 상세 페이지 중 본문 내용 구현 - 책 정보와 dabate 정보를 받아 표시 - 유저 아이콘 구현되면 host와 참여자 표시 가능

---
+++ src/components/MyDebateBody.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import moment from 'moment';
+import { Grid, Typography, Divider } from '@material-ui/core';
+import styled from 'styled-components';
+
+const MyDebateBody = ({ info }) => {
+  const { imgUrl, title, author, publisher, publshDate, tags, lastDebate, nextDebate, host, joinList } = info;
+
+  return (
+    <Grid container spacing={10} style={{ height: '450px' }}>
+      <GridStyled item xs={4} justify='center' alignItems='center'>
+        <BookCover src={imgUrl} alt='book cover' />
+      </GridStyled>
+      <GridStyled item xs={8} direction='column' justify='center'>
+        <Typography variant='h4' style={{ marginBottom: '15px' }}>
+          {title}
+        </Typography>
+        <BookinfoWrapper>
+          <Typography>Author: {author}</Typography>
+          <Divider light='true' orientation='vertical' variant='middle' />
+          <Typography>Publisher: {publisher}</Typography>
+          <Divider light='true' orientation='vertical' variant='middle' />
+          <Typography>Publish date: {publshDate}</Typography>
+        </BookinfoWrapper>
+        <Divider variant='middle' style={{ margin: '20px 0' }} />
+        <BookinfoWrapper style={{ margin: '10px 0 30px' }}>
+          {tags.map((el) => (
+            <Typography style={{ marginRight: '30px' }}>#{el}</Typography>
+          ))}
+        </BookinfoWrapper>
+        {lastDebate && (
+          <BookinfoWrapper style={{ marginBottom: '7px' }}>
+            <Typography>Last debate</Typography>
+            <Divider light='true' orientation='vertical' variant='middle' />
+            {moment(lastDebate).format('MMMM Do, H:mm')}
+          </BookinfoWrapper>
+        )}
+        {nextDebate && (
+          <BookinfoWrapper style={{ marginBottom: '40px' }}>
+            <Typography>Next debate</Typography>
+            <Divider light='true' orientation='vertical' variant='middle' />
+            {moment(nextDebate).format('MMMM Do, H:mm')}
+          </BookinfoWrapper>
+        )}
+        <BookinfoWrapper>
+          <ParticipantsWrapper style={{ width: '20%' }}>
+            <Typography variant='body2'>Host</Typography>
+            {/* TODO: Host Icons */}
+          </ParticipantsWrapper>
+          <ParticipantsWrapper style={{ width: '80%' }}>
+            <Typography variant='body2'>Participants</Typography>
+            {joinList.map((el) => (
+              <>{/* TODO: Participants Icons */}</>
+            ))}
+          </ParticipantsWrapper>
+        </BookinfoWrapper>
+      </GridStyled>
+    </Grid>
+  );
+};
+
+const GridStyled = styled(Grid)`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const BookCover = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const BookinfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: '30px';
+  max-width: 100%;
+`;
+
+const ParticipantsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default MyDebateBody;
