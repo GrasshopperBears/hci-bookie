b00a039712079087d1569762a777691771f6fac0
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-20 21:49:43 +0900

    세션 추가 컴포넌트 구현 - 인원, 날짜 추가 예정 - 제출 시 동작 추후 구현

---
+++ src/components/EnterBookInformation.js
@@ -0,0 +1,96 @@
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
+import { Button, TextField, Dialog, DialogTitle, List, ListItem, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+const EnterBookInformation = ({ setBookInfo }) => {
+  const [inputValue, setInputValue] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  const closeDialog = () => {
+    setVisible(false);
+    setBooks([]);
+  };
+
+  const searchHandler = async () => {
+    const headers = { Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_API_KEY}` };
+    try {
+      const result = await axios.get(`/v3/search/book?query=${inputValue}`, { headers });
+      setBooks(result.data.documents);
+      setVisible(true);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const selectBook = (book) => {
+    setInputValue(book.title);
+    setBookInfo(book);
+    closeDialog();
+  };
+
+  return (
+    <Wrapper>
+      <TextField
+        required
+        fullWidth
+        variant='outlined'
+        id='bookInfo'
+        label='Find Book'
+        placeholder='Enter book information'
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        value={inputValue}
+        InputProps={{
+          endAdornment: (
+            <Button onClick={searchHandler} variant='contained' size='large' color='primary'>
+              Find
+            </Button>
+          ),
+        }}
+      />
+      <Dialog open={visible} onClose={closeDialog} fullWidth maxWidth='sm'>
+        <DialogTitle>Select book</DialogTitle>
+        <List component='div' role='list'>
+          {books.map((book) => (
+            <ListItem
+              button
+              divider
+              onClick={() => {
+                selectBook(book);
+              }}
+              role='listitem'
+              key={book.isbn}
+            >
+              <BookCover src={book.thumbnail} />
+              <BookInfo>
+                <Typography variant='h6'>{book.title}</Typography>
+                <Typography variant='subtitle2'>{book.authors.join(', ')}</Typography>
+              </BookInfo>
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BookCover = styled.img`
+  height: 110px;
+  margin: 0 40px;
+`;
+
+const BookInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default EnterBookInformation;
--- src/pages/CreateSession.js
+++ src/pages/CreateSession.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import CeterDiv from '../components/CenterDiv';
+import EnterBookInformation from '../components/EnterBookInformation';
+import { Typography, FormControl, Button, TextField } from '@material-ui/core';
 
 const CreateSession = () => {
-  return <>CreateSession</>;
+  const [bookInfo, setBookInfo] = useState(undefined);
+  const title = useRef(undefined);
+  const memberNumber = useRef(undefined);
+  const briefDescription = useRef(undefined);
+  const dateTime = useRef(undefined);
+  const zoomUrl = useRef(undefined);
+  const content = useRef(undefined);
+
+  const submitHandler = () => {};
+
+  return (
+    <>
+      <Typography variant='h2'>Create session</Typography>
+      <form onSubmit={submitHandler}>
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            inputRef={title}
+            variant='outlined'
+            id='title'
+            label='Title'
+            placeholder='Enter title of sesssion'
+          />
+        </FormControl>
+        <EnterBookInformation setBookInfo={setBookInfo} />
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            multiline
+            inputRef={briefDescription}
+            rows={5}
+            rowsMax={5}
+            variant='outlined'
+            id='briefDescription'
+            label='Brief Description'
+            placeholder='Briefly write about the session'
+          />
+        </FormControl>
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            inputRef={title}
+            variant='outlined'
+            id='zoomUrl'
+            label='Zoom URL'
+            placeholder='Enter URL of ZOOM meeting room'
+          />
+        </FormControl>
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            multiline
+            inputRef={content}
+            rows={20}
+            rowsMax={20}
+            variant='outlined'
+            id='content'
+            label='Essay'
+            placeholder='Write down your essay. Please share how you think and feel.'
+          />
+        </FormControl>
+        <CeterDiv>
+          <Button type='submit' variant='contained' color='primary' size='large' style={{ float: 'center' }}>
+            Submit
+          </Button>
+        </CeterDiv>
+      </form>
+    </>
+  );
 };
 
 export default CreateSession;
