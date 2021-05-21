2ed6b85e303a81ca2ee4b265b649caf6639d62db
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-22 00:36:08 +0900

    로딩 페이지 구현 및 유저 초기화

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import firebase from './firebase-config';
 import MainRouter from './router';
+import Loading from './components/Loading';
 import './App.css';
 
 const App = () => {
-  return (
+  const [userInitialized, setUserInitialized] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!userInitialized) setUserInitialized(true);
+    });
+    return unsubscribe;
+  }, []);
+
+  return userInitialized ? (
     <div className='App' style={{ padding: '0 200px' }}>
       <MainRouter />
     </div>
+  ) : (
+    <Loading />
   );
 };
 
