a1d070eb4ccf4812d586f4c546b4dec6e2158ffc
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-22 01:45:27 +0900

    MyDebateDetail에 store 조회 로직 추가

--- src/pages/MyDebateDetail.js
+++ src/pages/MyDebateDetail.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import MyDebateBody from '../components/MyDebateBody';
+import Loading from '../components/Loading';
+import firebase from '../firebase-config';
 
-const info = {
-  imgUrl: 'https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg',
-  title: 'Book Title',
-  date: new Date(),
-  lastDebate: new Date(),
-  nextDebate: new Date(),
-  host: '123',
-  joinList: [],
-  description:
-    'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add    1 cup of frozen peas along with the mussels, if you like.arty dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
-  genre: 'Social',
-  peopleNumber: 7,
-  author: 'Jacoddddddddddddddddd',
-  publisher: 'PEARSON',
-  publshDate: '2020.15.14',
-  tags: ['S.F.', 'fun', 'novel'],
-};
 const MyDebateDetail = () => {
-  return (
+  const { id } = useParams();
+  const history = useHistory();
+  const [bookInfo, setBookInfo] = useState(undefined);
+
+  const fetchData = async () => {
+    const result = await firebase.firestore().collection('sessions').doc(id).get();
+    if (!result.exists) history.push('/');
+    setBookInfo(result.data());
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [id]);
+
+  return bookInfo ? (
     <div>
-      < MyDebateBody info={info} />
+      <MyDebateBody info={bookInfo} />
     </div>
+  ) : (
+    <Loading />
   );
 };
 
