87a75434afb66735ab25bc4be13d5d5dcf41a660
ccloe4 <ccloe4@kaist.ac.kr>
2021-05-21 16:09:41 +0900

    AddMyBookmark 수정 & router 오타 수정

--- src/pages/AddMyBookmark.js
+++ src/pages/AddMyBookmark.js
@@ -1,84 +1,65 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import CeterDiv from '../components/CenterDiv';
 import { TextField, Typography, FormControl, Button } from '@material-ui/core';
+import EnterBookInformation from '../components/EnterBookInformation';
+import firebase from '../firebase-config';
 
 const AddMyBookmark = () => {
-  const title = useRef(undefined);
+  const [bookInfo, setBookInfo] = useState(undefined);
   const comment = useRef(undefined);
   const review = useRef(undefined);
-  const author = useRef(undefined);
-  const publisher = useRef(undefined);
-
-  const submitHandler = (e) => {
+  // bookInfo 는 객체 형식으로 title, author, publisher ... 등등의 정보를 포함함.
+  const submitHandler = async (e) => {
     e.preventDefault();
-    // title.current, comment.current, review.current에 value 저장되어있음
+    if (!bookInfo) return alert('Please enter book information');
+    await firebase
+      .firestore()
+      .collection('bookshelf')
+      .add({
+        bookInfo,
+        comment: comment.current.value,
+        review: review.current.value,
+      });
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <>
       <Typography variant='h2'>Add New Book</Typography>
-      <FormControl fullWidth margin='normal'>
-        <TextField
-          required
-          inputRef={title}
-          variant='outlined'
-          id='title'
-          label='title'
-          placeholder='Search Book'
-        />
-      </FormControl>
-      <FormControl margin='normal' style={{ width: '50%', float: 'left' }}>
-        <TextField
-          required
-          inputRef={author}
-          variant='outlined'
-          id='author'
-          label='author'
-          placeholder='Author'
-        />
-      </FormControl>
-      <FormControl margin='normal' style={{ width: '50%', float: 'left' }}>
-        <TextField
-          required
-          inputRef={publisher}
-          variant='outlined'
-          id='publisher'
-          label='publisher'
-          placeholder='Publisher'
-        />
-      </FormControl>
-      <FormControl fullWidth margin='normal'>
-        <TextField
-          required
-          multiline
-          inputRef={comment}
-          rows={3}
-          rowsMax={5}
-          variant='outlined'
-          id='Comment'
-          label='Comment'
-          placeholder='Write down your Comment'
-        />
-      </FormControl>
-      <FormControl fullWidth margin='normal'>
-        <TextField
-          required
-          multiline
-          inputRef={review}
-          rows={20}
-          rowsMax={20}
-          variant='outlined'
-          id='Review'
-          label='Review'
-          placeholder='Write down your Review. Please share how you think and feel.'
-        />
-      </FormControl>
-      <CeterDiv>
-        <Button type='submit' variant='contained' color='primary' size='large' style={{ float: 'center' }}>
-          Post
-        </Button>
-      </CeterDiv>
-    </form>
+        <form onSubmit={submitHandler}>
+        <EnterBookInformation bookInfo={bookInfo} setBookInfo={setBookInfo} />
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            multiline
+            inputRef={comment}
+            rows={3}
+            rowsMax={5}
+            variant='outlined'
+            id='Comment'
+            label='Comment'
+            placeholder='Write down your Comment'
+          />
+        </FormControl>
+        <FormControl fullWidth margin='normal'>
+          <TextField
+            required
+            multiline
+            inputRef={review}
+            rows={20}
+            rowsMax={20}
+            variant='outlined'
+            id='Review'
+            label='Review'
+            placeholder='Write down your Review. Please share how you think and feel.'
+          />
+        </FormControl>
+        <CeterDiv>
+          <Button type='submit' variant='contained' color='primary' size='large' style={{ float: 'center' }}>
+            Post
+          </Button>
+        </CeterDiv>
+      </form>
+    </>
   );
 };
 
--- src/router.js
+++ src/router.js
@@ -8,24 +8,24 @@ const MainRouter = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Pages.MainPage} />
-        <UnauhorizedRouter exact path='/signup' component={Pages.Signup} />
-        <UnauhorizedRouter exact path='/signin' component={Pages.Signin} />
-        <AuhorizedRouter exact path='/my-debate/:id' component={Pages.MyDebateDetail} />
-        <AuhorizedRouter exact path='/session/create' component={Pages.CreateSession} />
+        <UnauthorizedRouter exact path='/signup' component={Pages.Signup} />
+        <UnauthorizedRouter exact path='/signin' component={Pages.Signin} />
+        <AuthorizedRouter exact path='/my-debate/:id' component={Pages.MyDebateDetail} />
+        <AuthorizedRouter exact path='/session/create' component={Pages.CreateSession} />
         <Route exact path='/session/detail/:id' component={Pages.SessionDetail} />
+        <AuthorizedRouter exact path='/bookshelf/add' component={Pages.AddMyBookmark} />
         <Route exact path='/bookshelf/:id' component={Pages.BookshelfMain} />
         <Route exact path='/bookshelf/detail/:id' component={Pages.BookshelfDetail} />
-        <AuhorizedRouter exact path='/bookshelf/add' component={Pages.AddMyBookmark} />
         <Route exact path='/shareboard/:id/ongoing' component={Pages.ShareboardOngoing} />
         <Route exact path='/shareboard/:id/history' component={Pages.ShareboardHistory} />
-        <AuhorizedRouter exact path='/shareboard/:id/add' component={Pages.AddEssay} />
+        <AuthorizedRouter exact path='/shareboard/:id/add' component={Pages.AddEssay} />
         <Route component={Pages.NotFound} />
       </Switch>
     </BrowserRouter>
   );
 };
 
-const AuhorizedRouter = ({ exact, path, component }) => {
+const AuthorizedRouter = ({ exact, path, component }) => {
   return firebase.auth().currentUser != null ? (
     <Route exact={exact} path={path} component={component} />
   ) : (
@@ -33,7 +33,7 @@ const AuhorizedRouter = ({ exact, path, component }) => {
   );
 };
 
-const UnauhorizedRouter = ({ exact, path, component }) => {
+const UnauthorizedRouter = ({ exact, path, component }) => {
   return firebase.auth().currentUser == null ? (
     <Route exact={exact} path={path} component={component} />
   ) : (
