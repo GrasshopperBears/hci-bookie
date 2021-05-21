2634033e7280be3e1add317e64d1b160617059f1
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-22 01:46:19 +0900

    보여주는 프로필 변경

--- src/components/UserInformation.js
+++ src/components/UserInformation.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import moment from 'moment';
-import { } from '@material-ui/core';
 import styled from 'styled-components';
-
 import { Grid, Typography } from '@material-ui/core';
 
-const UserInformation = ({ info }) => {
-    const { role, profileImg, name, quote } = info;
+const UserInformation = ({ isHost, info }) => {
+  const { profileImg, name, quote } = info;
 
-    return (
-        <Grid container spacing={1} >
-            <GridStyled item xs={12} direction='row' alignItems='center' justify='flex-start' justifyContent='flex-center'>
-                <Typography variant='h7' style={{ color: 'grey', margin: '0 20px 0 0' }}>
-                    {role}
-                </Typography>
-                <ProfileImg src={profileImg} style={{ margin: '10px 10px 10px 0' }} alt='book cover' />
-                <Typography variant='h8' style={{ color: 'black', fontWeight: 'bold', margin: '0 15px 0 0' }}>
-                    {name}
-                </Typography>
-                <Typography variant='h8' style={{ color: 'grey', fontStyle: 'italic', margin: '0 10px 0 0' }}>
-                    {quote}
-                </Typography>
-            </GridStyled>
-        </Grid >
-    );
+  return (
+    <Grid container spacing={1}>
+      <GridStyled
+        item
+        xs={12}
+        direction='row'
+        alignItems='center'
+        justify='flex-start'
+        justifyContent='flex-center'
+      >
+        <Typography variant='h7' style={{ color: 'grey', margin: '0 20px 0 0' }}>
+          {isHost ? 'Host' : 'Participant'}
+        </Typography>
+        <ProfileImg
+          src={profileImg || process.env.PUBLIC_URL + '/default-profile.png'}
+          style={{ margin: '10px 20px 10px 0' }}
+          alt='book cover'
+        />
+        <Typography variant='h8' style={{ color: 'black', fontWeight: 'bold', margin: '0 15px 0 0' }}>
+          {name || 'Anonymous user'}
+        </Typography>
+        <Typography variant='h8' style={{ color: 'grey', fontStyle: 'italic', margin: '0 10px 0 0' }}>
+          {quote || ''}
+        </Typography>
+      </GridStyled>
+    </Grid>
+  );
 };
 
 const GridStyled = styled(Grid)`
@@ -32,7 +40,7 @@ const GridStyled = styled(Grid)`
   width: 100%;
 `;
 const ProfileImg = styled.img`
-  max-width: 5%;
-  max-height: 5%;
+  max-width: 4%;
+  max-height: 4%;
 `;
 export default UserInformation;
