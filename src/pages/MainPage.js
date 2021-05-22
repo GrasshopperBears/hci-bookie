acfe50927501a8211243682ba46775ad65979a84
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-22 14:30:45 +0900

    hotfix: 로그인되지 않은 유저의 처리

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import RecentlyAddedSession from '../components/RecentlyAddedSession';
 import MainPageMySessions from '../components/MainPageMySessions';
+import firebase from '../firebase-config';
 
 const MainPage = () => {
   return (
     <div>
-      <MainPageMySessions />
+      {firebase.auth().currentUser && <MainPageMySessions />}
       <RecentlyAddedSession />
     </div>
   );
