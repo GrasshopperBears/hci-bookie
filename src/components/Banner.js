f48da899c7017f55d0a76247f12b86e72e4c93ee
HwangYoungzu <63434619+HwangYoungzu@users.noreply.github.com>
2021-05-22 15:20:12 +0900

    배너 로고 및 선 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from 'react-router-dom';
 import { GoMegaphone } from 'react-icons/go';
 import { ImBooks } from 'react-icons/im';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+const font = "'Russo One', sans-serif";
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: font,
+        textTransform: "none"
+    }
+});
 const Banner = () => {
     const classes = useStyles();
     const history = useHistory();
@@ -13,7 +21,7 @@ const Banner = () => {
     const bookshelf = "/bookshelf/:id";
 
     const bookie = () => {
-      history.push(main);
+        history.push(main);
     };
 
     const opendebate = () => {
@@ -26,19 +34,19 @@ const Banner = () => {
 
     // top padding +40px 위치에 button 위치하게 함. Ex) paddingTop: 50px => button top: 90px; paddingTop: 100px => button top: 140px,
     return (
-        <Grid style={{paddingTop:'30px'}}>
+        <Grid style={{ paddingTop: '30px' }}>
             <Button className={classes.button} onClick={bookie}>
-            <Typography className={classes.text} variant="h2">
-                Bookie
-            </Typography>
+                <Typography className={classes.banner} variant="h2">
+                    Bookie
+                </Typography>
             </Button>
-            <Button className={classes.button} onClick={opendebate} style={{position: 'absolute', left: 'auto', right: '350px', top: '70px'}} startIcon={<GoMegaphone className={classes.icon}/>}>
+            <Button className={classes.button} onClick={opendebate} style={{ position: 'absolute', left: 'auto', right: '350px', top: '70px' }} startIcon={<GoMegaphone className={classes.icon} />}>
                 Open Debate
             </Button>
-            <Button className={classes.button} onClick={mybookshelf} style={{position: 'absolute', left: 'auto', right: '200px', top: '70px'}}startIcon={<ImBooks className={classes.icon}/>}>
+            <Button className={classes.button} onClick={mybookshelf} style={{ position: 'absolute', left: 'auto', right: '200px', top: '70px' }} startIcon={<ImBooks className={classes.icon} />}>
                 My Bookshelf
             </Button>
-            <Divider className={classes.divider} orientation='horizontal'></Divider>
+            <HorizonLine w='100%' m='10px 0 20px 0' b='5px solid #EC9F05' />
         </Grid>
     );
 };
@@ -56,6 +64,23 @@ const useStyles = makeStyles({
     icon: {
         color: "#EC9f05"
     },
+    banner: {
+        color: "#EC9F05",
+        fontFamily: font
+    }
 });
-
+const HorizonLine = ({ w, m, b }) => {
+    return (
+        <div
+            style={{
+                width: w,
+                textAlign: 'center',
+                align: 'center',
+                borderBottom: b,
+                lineHeight: '0.1em',
+                margin: m,
+            }}
+        ></div>
+    );
+};
 export default Banner;
